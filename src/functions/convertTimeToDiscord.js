1eed49c824871dfc95998511fa79bdcd856d89bd
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2024-02-04 00:21:55 +0530

    chore(convertTimeToDiscord): type checking

--- src/functions/convertTimeToDiscord.js
+++ src/functions/convertTimeToDiscord.js
@@ -1,3 +1,5 @@
+import { expectedTypeError } from "../error";
+
 /**
  * Convert a unix epoch time to Discord timestamps.
  * @see {@link https://bit.ly/discord-timestamps} for information about Discord timestamps.
@@ -8,5 +10,7 @@
  * doodle.convertTimeToDiscord(Date.now() - 3000, "R") // When posted on Discord, shows 3 seconds ago
  */
 export default function convertTimeToDiscord(time, type) {
+    if (typeof time !== "number") throw expectedTypeError("time", "number");
+    if (typeof type !== "string") throw expectedTypeError("time", "string");
     return `<t:${Math.floor(time / 1000)}${type ? `:${type}` : ""}>`;
 }
